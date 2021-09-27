df9655449edde54c691256f95dda82ef2251b1fb
ChristianWalmar <edbuiskindaawesome@gmail.com>
2021-09-27 16:46:48 +0200

    Added spring method for seeing if it's friday or not

--- HelloWorldSpring/src/main/java/controllers/myEndPoints.java
+++ HelloWorldSpring/src/main/java/controllers/myEndPoints.java
@@ -6,26 +6,52 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+
 @SpringBootApplication
 @RestController
 public class myEndPoints {
-    public static void main(String[] args) {
-        SpringApplication.run(myEndPoints.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(myEndPoints.class, args);
+  }
 
-    @GetMapping("friendlyMessage")
-    public String friendlyMessage(){
-       return "Velkommen til dette site!";
-    }
+  @GetMapping("friendlyMessage")
+  public String friendlyMessage() {
+    return "Velkommen til dette site!";
+  }
 
-    @GetMapping("/returnMyEcho")
-    public String echo(@RequestParam String echo){
-        return echo;
-    }
+  @GetMapping("/returnMyEcho")
+  public String echo(@RequestParam String echo) {
+    return echo;
+  }
 
-   @GetMapping("erDetFredag")
-    public String echo(){
-        String s = "Er det fredag?";
-        return s;
+  @GetMapping("erDetFredag")
+  public String echo() {
+    String s = "Er det fredag?";
+    return s;
+  }
+
+  @GetMapping("erDetEgentligFredag")
+  public String erDetEgentligFredag() {
+
+    /*
+    **************************************************************************************************
+    SPØRGSMÅL TIL NICKLAS: Hvordan ville man kunne unit teste det her? Uden at vente til fredag...
+    **************************************************************************************************
+    */
+    LocalDateTime localDateTime = LocalDateTime.now();
+
+    int d = localDateTime.getDayOfMonth(); //day
+    int m = localDateTime.getMonthValue(); //month
+    int k = localDateTime.getYear()%100;   //last 2 digits of the given year
+    int j = localDateTime.getYear()/100;   //century
+
+    int h = d + 13 * (m + 1) / 5 + k + k / 4 + j / 4 + 5 * j;
+
+    if (h == 5) {
+      return "ja";
+    } else{
+      return "nej";
     }
+  }
 }
